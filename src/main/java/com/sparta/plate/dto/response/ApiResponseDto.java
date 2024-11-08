f594ae221c70912ed0fed937b1ac25fa06ec79d8
Misu <misuhan33@gmail.com>
2024-11-08 15:12:51 +0900

    [feat] ApiResponse dto 생성

---
+++ src/main/java/com/sparta/plate/dto/response/ApiResponseDto.java
@@ -0,0 +1,19 @@
+package com.sparta.plate.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponseDto {
+    private int statusCode;
+    private String statusMessage;
+    private String message;
+    private Object data;
+}
