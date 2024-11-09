e4515f9586898e97562ee077b2b25813af97f1dd
eunjeong <jjj3080@naver.com>
2024-11-10 06:31:36 +0900

    Timestamped 수정

--- src/main/java/com/sparta/plate/entity/Timestamped.java
+++ src/main/java/com/sparta/plate/entity/Timestamped.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Getter
 @MappedSuperclass
@@ -17,34 +18,28 @@ public abstract class Timestamped {
     // 생성일
     @CreatedDate
     @Column(updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
     // 생성자
     @CreatedBy
     @Column(updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    private String createBy;
+    private UUID createBy;
 
     // 수정일
     @LastModifiedDate
     @Column
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime updateAt;
 
     // 수정자
     @LastModifiedBy
     @Column
-    @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime updateBy;
+    private UUID updateBy;
 
     // 삭제일
-    @Temporal(TemporalType.TIMESTAMP)
     @Column
     private LocalDateTime deletedAt;
 
     // 삭제자
-    @Temporal(TemporalType.TIMESTAMP)
     @Column
-    private LocalDateTime deletedBy;
+    private UUID deletedBy;
 }
