89d00e3f1838d1415a22e78d3aa44e9041b42633
eunjeong <jjj3080@naver.com>
2024-11-10 04:40:02 +0900

    payment dto 작성

---
+++ src/main/java/com/sparta/plate/dto/request/PaymentRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.plate.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PaymentRequestDto {
+    private Long orderId;
+    private Long amount;
+}
