c08f194acaac7a8740a909fc6c09cacb2d9ee5ad
Misu <misuhan33@gmail.com>
2024-11-09 22:51:05 +0900

    [refactor] ProductDisplayStatus enum 데이터 변경

--- src/main/java/com/sparta/plate/entity/ProductDisplayStatus.java
+++ src/main/java/com/sparta/plate/entity/ProductDisplayStatus.java
@@ -3,8 +3,6 @@ package com.sparta.plate.entity;
 public enum ProductDisplayStatus {
     PENDING_SALE,
     IN_STOCK,
-    SOLD_OUT_TODAY,
-    OUT_OF_STOCK,
     DISCONTINUED;
 
     public static ProductDisplayStatus fromString(String status) {
