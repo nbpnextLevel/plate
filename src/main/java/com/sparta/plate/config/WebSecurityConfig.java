217436e26d9234879bd00a556055b9734734101e
jaehee-An <jhh9615@gmail.com>
2024-11-11 01:20:03 +0900

    [feat] security config 수정 (임시로 모든 api에 대해 권한 허용)

--- src/main/java/com/sparta/plate/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/plate/config/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig {
 			authorizeHttpRequests
 				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
 				.requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-				.requestMatchers("/api/users/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+			//	.requestMatchers("/api/users/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
 				.requestMatchers("/api/**").permitAll() // '/api/'로 시작하는 요청 모두 접근 허가 TODO 시큐리티 완성 후 수정할 예정 - 재희
 				.anyRequest().authenticated() // 그 외 모든 요청 인증처리
 		);
