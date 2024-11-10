c222b96dc8c00a2c45d74cc797531d59c2d234ab
jaehee-An <jhh9615@gmail.com>
2024-11-11 01:20:34 +0900

    [feat] 가게 카테고리 조회 api 구현

---
+++ src/main/java/com/sparta/plate/service/StoreCategoryService.java
@@ -0,0 +1,21 @@
+package com.sparta.plate.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sparta.plate.entity.StoreCategory;
+import com.sparta.plate.repository.StoreCategoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StoreCategoryService {
+
+	private final StoreCategoryRepository storeCategoryRepository;
+
+	public List<StoreCategory> getAllStoreCategories() {
+		return storeCategoryRepository.findAll();
+	}
+}
