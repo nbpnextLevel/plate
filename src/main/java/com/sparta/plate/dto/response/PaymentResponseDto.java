6d33e79d4496e95d2db1e9aa345c62023d83930f
eunjeong <jjj3080@naver.com>
2024-11-11 18:33:13 +0900

    [feat] payment dto 수정

--- src/main/java/com/sparta/plate/dto/response/PaymentResponseDto.java
+++ src/main/java/com/sparta/plate/dto/response/PaymentResponseDto.java
@@ -1,21 +1,32 @@
 package com.sparta.plate.dto.response;
 
+import com.sparta.plate.entity.Payment;
 import com.sparta.plate.entity.Timestamped;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentResponseDto {
-    private Long paymentId;
-    private Long orderId;
+    private UUID paymentId;
+    private UUID orderId;
     private String paymentNumber;
     private Boolean isPaid;
     private Long amount;
 
     private Timestamped timestamped;
+
+    public PaymentResponseDto(Payment payment) {
+        this.paymentId = payment.getPaymentId();
+        this.orderId = payment.getOrder().getOrderId();
+        this.paymentNumber = payment.getPaymentNumber();
+        this.isPaid = payment.isPaid();
+        this.amount = payment.getAmount();
+    }
 }
 
