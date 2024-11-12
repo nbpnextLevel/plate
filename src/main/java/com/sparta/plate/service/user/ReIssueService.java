84f728274cbf763930029098d622fb55960b174a
jaehee-An <jhh9615@gmail.com>
2024-11-12 12:17:15 +0900

    [feat] refresh rotate 구현

--- src/main/java/com/sparta/plate/service/user/ReIssueService.java
+++ src/main/java/com/sparta/plate/service/user/ReIssueService.java
@@ -1,13 +1,10 @@
 package com.sparta.plate.service.user;
 
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
 import com.sparta.plate.entity.UserRoleEnum;
 import com.sparta.plate.jwt.JwtTokenProvider;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import jdk.swing.interop.SwingInterOpUtils;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -17,23 +14,25 @@ public class ReIssueService {
 	private final JwtTokenProvider jwtTokenProvider;
 
 	public String reissueAccessToken(String refreshToken) {
-		validateRefreshToken(refreshToken);
+		validateToken(refreshToken);
 
 		String loginId = jwtTokenProvider.getLoginIdFromToken(refreshToken);
-		System.out.println("login id: hhhh = " + loginId);
 		String role = jwtTokenProvider.getRoleFromToken(refreshToken);
-		role = role.replace("ROLE_", "");
 
-		return jwtTokenProvider.createAccessToken(loginId, UserRoleEnum.valueOf(role));
+		return jwtTokenProvider.createAccessToken(loginId, UserRoleEnum.valueOf(role.replace("ROLE_", "")));
 	}
 
-	private void validateRefreshToken(String refreshToken) {
-		try {
-			jwtTokenProvider.isExpired(refreshToken);
-		} catch (ExpiredJwtException e) {
-			throw new IllegalArgumentException("Refresh Token is expired");
-		}
+	public String reissueRefreshToken(String refreshToken) {
+		validateToken(refreshToken);
 
+		String loginId = jwtTokenProvider.getLoginIdFromToken(refreshToken);
+		String role = jwtTokenProvider.getRoleFromToken(refreshToken);
+
+		return jwtTokenProvider.createRefreshToken(loginId, UserRoleEnum.valueOf(role.replace("ROLE_", "")));
+	}
+
+	private void validateToken(String refreshToken) {
+		jwtTokenProvider.validateToken(refreshToken);
 		if(!jwtTokenProvider.isRefreshToken(refreshToken)) {
 			throw new IllegalArgumentException("invalid refresh token");
 		}
