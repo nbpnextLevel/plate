98dc78bf00ac9a067a1fbf3172f530256c9bb528
eunjeong <jjj3080@naver.com>
2024-11-12 17:02:50 +0900

    [feat] 결제 API 수정 필요 - TIMESTAMP 추가 - 권한 추가 - 결제 권한별 조회 API 수정 - 수정 필요 - 조회 버그

--- src/main/java/com/sparta/plate/dto/request/PaymentRequestDto.java
+++ src/main/java/com/sparta/plate/dto/request/PaymentRequestDto.java
@@ -1,26 +1,70 @@
-package com.sparta.plate.dto.request;
+package com.sparta.plate.dto.response;
 
-import lombok.AllArgsConstructor;
+import com.sparta.plate.entity.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
+
 @Getter
-@Setter
 @NoArgsConstructor
-public class PaymentRequestDto {
+public class PaymentResponseDto {
 
+    private UUID paymentId;
     private UUID orderId;
-    private Long orderPrice;
-    private boolean isCanceled;
+    private String paymentNumber;
+    private Boolean isPaid;
+    private Long amount;
 
-    public PaymentRequestDto(UUID orderId, Long orderPrice, boolean isCanceled) {
-        this.orderId = orderId;
-        this.orderPrice = orderPrice;
-        this.isCanceled = isCanceled;
+    // 가게명, 결제 상품, 주문 유형, 주문 주소, 결제 금액
+    private String storeName;
+    private List<OrderProduct> orderProductList;
+    private OrderTypeEnum orderTypeEnum;
+    private String address;
+    private Long orderPrice;
+
+    private UUID storeId;
+
+    private Timestamped timestamped;
+
+
+    public PaymentResponseDto(Order order, Payment payment) {
+        this.paymentId = payment.getPaymentId();
+        this.orderId = payment.getOrder().getOrderId();
+        this.storeId = payment.getOrder().getStore().getId();
+
+        this.paymentNumber = payment.getPaymentNumber();
+        this.isPaid = payment.isPaid();
+        this.amount = payment.getAmount();
+
+        this.storeName = payment.getOrder().getStore().getStoreName();
+        this.orderProductList = payment.getOrder().getOrderProductList();
+        this.orderTypeEnum = payment.getOrder().getOrderType();
+        this.address = payment.getOrder().getOrderAddress();
+        this.orderPrice = payment.getOrder().getOrderPrice();
+
+
+        this.timestamped = getTimestamped();
     }
 
 
+    public PaymentResponseDto(Payment payment) {
+        this.paymentId = payment.getPaymentId();
+        this.orderId = payment.getOrder().getOrderId();
+        this.storeId = payment.getOrder().getStore().getId();
+
+        this.paymentNumber = payment.getPaymentNumber();
+        this.isPaid = payment.isPaid();
+        this.amount = payment.getAmount();
+
+        this.storeName = payment.getOrder().getStore().getStoreName();
+        this.orderProductList = payment.getOrder().getOrderProductList();
+        this.orderTypeEnum = payment.getOrder().getOrderType();
+        this.address = payment.getOrder().getOrderAddress();
+        this.orderPrice = payment.getOrder().getOrderPrice();
+
+        this.timestamped = getTimestamped();
+    }
 }
