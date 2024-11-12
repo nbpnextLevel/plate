f74016a23a891fb2811e9128da8c532fe183fe90
jaehee-An <jhh9615@gmail.com>
2024-11-12 17:31:53 +0900

    [feat] 공통응답객체 수정

--- src/main/java/com/sparta/plate/dto/response/ApiResponseDto.java
+++ src/main/java/com/sparta/plate/dto/response/ApiResponseDto.java
@@ -8,14 +8,37 @@ import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ApiResponseDto {
+public class ApiResponseDto<T> {
     private int statusCode;
     private String statusMessage;
     private String message;
-    private Map<String, Object> data;
+    private Map<String, Object> content;
+
+    private T data;
+
+    // 메시지 없는 성공 응답
+    public static <T> ApiResponseDto<T> success(T data) {
+        return ApiResponseDto.<T>builder()
+            .statusCode(HttpStatus.OK.value())
+            .statusMessage(HttpStatus.OK.getReasonPhrase())
+            .data(data)
+            .build();
+    }
+
+    // 메시지를 포함한 성공 응답
+    public static <T> ApiResponseDto<T> success(String message, T data) {
+        return ApiResponseDto.<T>builder()
+            .statusCode(HttpStatus.OK.value())
+            .statusMessage(HttpStatus.OK.getReasonPhrase())
+            .message(message)
+            .data(data)
+            .build();
+    }
 }
