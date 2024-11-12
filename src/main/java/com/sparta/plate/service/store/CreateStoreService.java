0f1fdc80fb5633ad395706957e1a29821506a749
jaehee-An <jhh9615@gmail.com>
2024-11-12 17:41:22 +0900

    [feat] 가게 API 유저 반영

--- src/main/java/com/sparta/plate/service/store/CreateStoreService.java
+++ src/main/java/com/sparta/plate/service/store/CreateStoreService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import com.sparta.plate.dto.request.CreateStoreRequestDto;
 import com.sparta.plate.entity.Store;
 import com.sparta.plate.entity.StoreCategory;
+import com.sparta.plate.entity.User;
+import com.sparta.plate.exception.StoreCategoryNotFoundException;
 import com.sparta.plate.repository.StoreCategoryRepository;
 import com.sparta.plate.repository.StoreRepository;
 
@@ -17,27 +19,26 @@ public class CreateStoreService {
 	private final StoreRepository storeRepository;
 	private final StoreCategoryRepository storeCategoryRepository;
 
-	public Store createStore(CreateStoreRequestDto request) {
+	public Store createStore(CreateStoreRequestDto request, User user) {
 
 		StoreCategory storeCategory = findStoreCategoryById(request);
 
 		Store store = Store.builder()
-			.storeName(request.getName())
-			.address(request.getAddress())
-			.storeNumber(request.getStoreNumber())
+			.user(user)
 			.storeCategory(storeCategory)
+			.storeName(request.getName())
+			.storeNumber(request.getStoreNumber())
+			.address(request.getAddress())
 			.build();
 
-		Store savedStore = storeRepository.saveAndFlush(store);
-
-		// TODO Store set createdBy 필요
+		Store savedStore = storeRepository.save(store);
 
 		return savedStore;
 	}
 
 	private StoreCategory findStoreCategoryById(CreateStoreRequestDto request) {
 		return storeCategoryRepository.findById(request.getCategoryId())
-			.orElseThrow(() -> new IllegalArgumentException("Invalid category id"));
+			.orElseThrow(() -> new StoreCategoryNotFoundException("Store Category not found with ID: " + request.getCategoryId()));
 	}
 
 }
