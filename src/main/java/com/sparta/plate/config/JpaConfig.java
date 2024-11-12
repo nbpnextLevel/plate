c3170baea2227bbd5beb8b2d2e3e12d62aabbc1d
jaehee-An <jhh9615@gmail.com>
2024-11-12 16:00:14 +0900

    [feat] JpaConfig 설정

--- src/main/java/com/sparta/plate/config/JpaConfig.java
+++ src/main/java/com/sparta/plate/config/JpaConfig.java
@@ -1,10 +1,18 @@
 package com.sparta.plate.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.sparta.plate.config.audit.AuditorAwareImpl;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
 
+	@Bean
+	public AuditorAware<Long> auditorProvider() {
+		return new AuditorAwareImpl();
+	}
 }
