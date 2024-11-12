f5a866d342e29b0f0745369bcab34122e31c7454
eunjeong <jjj3080@naver.com>
2024-11-12 17:02:50 +0900

    [payment test] 결제 요청 API 테스트 - Order 수정 (주석)

--- src/main/java/com/sparta/plate/entity/Order.java
+++ src/main/java/com/sparta/plate/entity/Order.java
@@ -24,34 +24,34 @@ public class Order extends Timestamped{
     private UUID orderId;  // 주문 ID (UUID)
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)  // 외래 키 설정 (user_id)
+    @JoinColumn(name = "user_id")  // 외래 키 설정 (user_id)
     private User user;  // 주문한 사용자 (User 엔티티와 연관)
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "store_id", nullable = false)  // 외래 키 설정 (store_id)
+    @JoinColumn(name = "store_id")  // 외래 키 설정 (store_id)
     private Store store;  // 주문이 발생한 상점 (Store 엔티티와 연관)
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "order_type", nullable = false)
+    @Column(name = "order_type")
     private OrderTypeEnum orderType;  // 주문 유형 (ENUM)
 
-    @Column(name = "order_price", nullable = false)
+    @Column(name = "order_price")
     private Long orderPrice;  // 주문 금액 (NUMERIC(10,0))
 
-    @Column(name = "is_canceled", nullable = false)
+    @Column(name = "is_canceled")
     private Boolean isCanceled;  // 주문 취소 여부 (BOOLEAN)
 
-    @Column(name = "order_address", nullable = false, length = 225)
+    @Column(name = "order_address", length = 225)
     private String orderAddress;  // 배송지 주소 (VARCHAR(225))
 
     @Column(name = "order_request", length = 500)
     private String orderRequest;  // 주문 요청 사항 (VARCHAR(500))
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "order_status", nullable = false)
+    @Column(name = "order_status")
     private OrderStatusEnum orderStatus;  // 주문 상태 (ENUM)
 
-    @Column(name = "is_deleted", nullable = false)
+    @Column(name = "is_deleted")
     private Boolean isDeleted;  // 삭제 여부 (BOOLEAN)
 
     @OneToMany(mappedBy = "order")  // Order 엔티티에서 'product' 필드를 관리
@@ -69,6 +69,28 @@ public class Order extends Timestamped{
         this.orderRequest = requestDto.getOrderRequest();
         this.orderStatus= requestDto.getOrderStatus();
         this.orderProductList = orderProductList;
+    }
+
+
+    // payment Test용
+    public Order(UUID orderId, User user, Store store, OrderTypeEnum orderType, Long orderPrice, Boolean isCanceled, String orderAddress, String orderRequest, OrderStatusEnum orderStatus, Boolean isDeleted, List<OrderProduct> orderProductList) {
+//      this.orderId = orderId;
+        this.orderId = orderId;
+        this.user = user;
+        this.store = store;
+        this.orderType = orderType;
+        this.orderPrice = orderPrice;
+        this.isCanceled = isCanceled;
+        this.orderAddress = orderAddress;
+        this.orderRequest = orderRequest;
+        this.orderStatus = orderStatus;
+        this.isDeleted = isDeleted;
+        this.orderProductList = orderProductList;
+    }
+
+    // payment TEST용
+    public Order(UUID uuid, Object o, Object o1, Object o2, long l, boolean b, String 서울, String orderRequest, Object o3, Object o4) {
+        super();
     }
 
     public void addOrderProduct(OrderProduct orderProduct) {
