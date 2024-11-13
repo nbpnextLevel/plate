30d7ff796a4e2dcf6ce638b7ea2e27b7480a3f74
Misu <misuhan33@gmail.com>
2024-11-13 18:57:29 +0900

    [refactor] query dto 변경

--- src/main/java/com/sparta/plate/dto/request/ProductImageQueryDto.java
+++ src/main/java/com/sparta/plate/dto/request/ProductImageQueryDto.java
@@ -1,13 +1,20 @@
 package com.sparta.plate.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductImageQueryDto {
     private UUID id;
+    private UUID productId;
     private String isDeleted;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
--- src/main/java/com/sparta/plate/dto/request/ProductQueryDto.java
+++ src/main/java/com/sparta/plate/dto/request/ProductQueryDto.java
@@ -1,11 +1,17 @@
 package com.sparta.plate.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductQueryDto {
     private UUID storeId;
     private UUID productId;
