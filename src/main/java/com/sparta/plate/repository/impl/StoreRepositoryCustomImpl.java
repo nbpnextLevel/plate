b3d6603c09c9403890085ed4b033e8fa5f3a6278
jaehee-An <jhh9615@gmail.com>
2024-11-14 15:32:06 +0900

    [feat] 지점 검색시 queryDsl 활용

---
+++ src/main/java/com/sparta/plate/repository/impl/StoreRepositoryCustomImpl.java
@@ -0,0 +1,69 @@
+package com.sparta.plate.repository.impl;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.util.StringUtils;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.plate.dto.request.StoreSearchRequestDto;
+import com.sparta.plate.entity.QStore;
+import com.sparta.plate.entity.Store;
+import com.sparta.plate.repository.StoreRepositoryCustom;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class StoreRepositoryCustomImpl implements StoreRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Page<Store> searchStores(StoreSearchRequestDto request) {
+        QStore store = QStore.store;
+
+        // 기본 조건: isDeleted = false
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(store.isDeleted.eq(false));
+
+        // 카테고리 입력되면 해당 카테고리의 모든 지점 노출
+        if(request.getCategoryId() != null) {
+            builder.and(store.storeCategory.id.eq(request.getCategoryId()));
+        }
+
+        // 지점 입력되면 검색한 지점 이름 포함하는거 노출
+        if (StringUtils.hasText(request.getStoreName())) {
+            builder.and(store.storeName.contains(request.getStoreName()));
+        }
+
+        long total = getTotalCount(store, builder);
+        OrderSpecifier<?> orderSpecifier = getOrderBy(request, store);
+
+        List<Store> datas = jpaQueryFactory
+            .selectFrom(store)
+            .where(builder)
+            .orderBy(orderSpecifier)
+            .offset(request.getPageable().getOffset())
+            .limit(request.getPageable().getPageSize())
+            .fetch();
+
+        return new PageImpl<>(datas, request.getPageable(), total);
+    }
+
+    private long getTotalCount(QStore store, BooleanBuilder builder) {
+        return jpaQueryFactory
+            .selectFrom(store)
+            .where(builder)
+            .fetchCount();
+    }
+
+    private OrderSpecifier<?> getOrderBy(StoreSearchRequestDto request, QStore store) {
+        if ("createdAt".equals(request.getSortBy())) {
+            return request.isAsc() ? store.createdAt.asc() : store.createdAt.desc();
+        } else {
+            return request.isAsc() ? store.updateAt.asc() : store.updateAt.desc();
+        }
+    }
+}
--- src/main/java/com/sparta/plate/service/store/GetStoreListService.java
+++ src/main/java/com/sparta/plate/service/store/GetStoreListService.java
@@ -1,14 +1,11 @@
 package com.sparta.plate.service.store;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sparta.plate.dto.request.StoreSearchRequestDto;
 import com.sparta.plate.dto.response.StoreResponseDto;
-import com.sparta.plate.entity.Store;
 import com.sparta.plate.repository.StoreRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,24 +17,10 @@ public class GetStoreListService {
 	private final StoreRepository storeRepository;
 
 	@Transactional(readOnly = true)
-	public Page<StoreResponseDto> getStoreList(int page, int size, String sortBy, boolean isAsc, String search) {
+	public Page<StoreResponseDto> getStoreList(StoreSearchRequestDto request) {
 
-		// TODO 페이징 유틸로 만들기
-		// sortBy : createdAt, updatedAt만 가능
-		// 페이지사이즈 10, 30, 50 이외는 제한
-		Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
-		Sort sort = Sort.by(direction, sortBy);
-		Pageable pageable = PageRequest.of(page, size, sort);
+		request.validate();
 
-		Page<Store> storeList;
-
-		if(search == null || search.trim().isEmpty()) {
-			storeList = storeRepository.findByIsDeletedFalse(pageable);
-		} else {
-			storeList = storeRepository.findByStoreNameContainingAndIsDeletedFalse(pageable, search);
-		}
-
-		return storeList.map(StoreResponseDto::of);
+		return storeRepository.searchStores(request).map(StoreResponseDto::of);
 	}
-
 }
