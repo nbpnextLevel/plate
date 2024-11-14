f4d0002baed7edb0e4e479b702ed19e3ceb7d35c
jaehee-An <jhh9615@gmail.com>
2024-11-14 12:11:41 +0900

    [refact] 유저, 가게 관계 1:1로 수정

--- src/main/java/com/sparta/plate/entity/Store.java
+++ src/main/java/com/sparta/plate/entity/Store.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -27,7 +28,11 @@ public class Store extends Timestamped {
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "user_id", nullable = false)
+	// private User user;
+
+	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
