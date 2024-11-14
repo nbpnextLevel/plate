4bfb8e4d93499753d5b7385bc88b756c283f1d78
jaehee-An <jhh9615@gmail.com>
2024-11-14 14:31:24 +0900

    [feat] 로그아웃 기능

--- src/main/java/com/sparta/plate/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/plate/config/WebSecurityConfig.java
@@ -1,5 +1,10 @@
 package com.sparta.plate.config;
 
+import com.sparta.plate.jwt.JwtFilter;
+import com.sparta.plate.jwt.JwtTokenProvider;
+import com.sparta.plate.jwt.LoginFilter;
+import com.sparta.plate.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,61 +19,55 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.sparta.plate.jwt.JwtFilter;
-import com.sparta.plate.jwt.LoginFilter;
-import com.sparta.plate.jwt.JwtTokenProvider;
-import com.sparta.plate.security.UserDetailsServiceImpl;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-	private final JwtTokenProvider jwtTokenProvider;
-	private final UserDetailsServiceImpl userDetailsService;
-	private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-		return configuration.getAuthenticationManager();
-	}
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
-	@Bean
-	public LoginFilter loginFilter() throws Exception {
-		LoginFilter filter = new LoginFilter(jwtTokenProvider);
-		filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
-		return filter;
-	}
+    @Bean
+    public LoginFilter loginFilter() throws Exception {
+        LoginFilter filter = new LoginFilter(jwtTokenProvider);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
 
-	@Bean
-	public JwtFilter jwtFilter() throws Exception {
-		return new JwtFilter(jwtTokenProvider, userDetailsService);
-	}
+    @Bean
+    public JwtFilter jwtFilter() throws Exception {
+        return new JwtFilter(jwtTokenProvider, userDetailsService);
+    }
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		//csrf disable
-		http.csrf((csrf) -> csrf.disable());
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // csrf disable
+        http.csrf((csrf) -> csrf.disable());
 
-		//From 로그인 방식 disable
-		http
-			.formLogin((auth) -> auth.disable());
+        // From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
 
-		//http basic 인증 방식 disable
-		http
-			.httpBasic((auth) -> auth.disable());
+        // http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
 
-		// 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
-		http.sessionManagement((sessionManagement) ->
-			sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		);
+        // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http.sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
 
         // TODO 각자 권한에 따른 설정 필요
         http.authorizeHttpRequests((authorizeHttpRequests) ->
